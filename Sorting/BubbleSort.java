9b3b74ccb4047cadba5f22a494f3c45a75a8d67c
Raushan <35721857+kolotushkina@users.noreply.github.com>
2018-09-11 08:58:44 +0300

    BubbleSort added

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,1 +1,46 @@
+package Sorting;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int [] nums={43,12,3,-54,34,908,4,-2,6};
+		
+		System.out.println("Before sorting:");
+		for(int num: nums)
+		{
+			System.out.println(num);
+		}
+		
+		nums=sort(nums);
+		
+		System.out.println("After sorting:");
+		for(int num: nums)
+		{
+			System.out.println(num);
+		}
+		
+		System.out.println("Done!");
+
+	}
+
+	private static int[] sort(int[] nums) {
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=0;j<nums.length-1;j++)
+			{
+				if(nums[j]>nums[j+1])
+				{
+					int temp=nums[j+1];
+					nums[j+1]=nums[j];
+					nums[j]=temp;					
+				}
+			}
+		}
+		
+		return nums;
+	}
+
+}
 
